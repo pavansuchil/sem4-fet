30405da5e80dc00b7cd27133b32e303fffd79849
pavansuchil <pavanuchil2012@gmail.com>
2026-04-25 12:49:24 +0530

    Update script.js

--- assignment/script.js
+++ assignment/script.js
@@ -1,1 +1,15 @@
+function editRole() {
+   
+    alert("You are about to change your role!");
 
+   
+    let newRole = prompt("Enter your new role:");
+
+    
+    if (newRole !== null && newRole.trim() !== "") {
+        document.getElementById("role").innerText = newRole;
+        alert("Role updated successfully!");
+    } else {
+        alert("No changes made.");
+    }
+}

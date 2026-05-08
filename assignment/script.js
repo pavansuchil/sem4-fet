1ff900be9260b5a9d0b55b5a0b14a90113aa6845
pavansuchil <pavanuchil2012@gmail.com>
2026-05-08 21:06:50 +0530

    Update script.js

--- assignment/script.js
+++ assignment/script.js
@@ -1,15 +1,27 @@
+window.onload = function () {
+    alert("welcome to kishan's portfolio!");
+};
+
+document.getElementById("welcomeBtn").addEventListener(
+    "click",
+    function () {
+        alert("Thanks for visiting my portfolio!");
+    }
+);
+
+document.getElementById("a1").addEventListener(
+    "click",
+    function () {
+        this.style.color = "darkgreen";
+    }
+);
+
 function editRole() {
-   
-    alert("You are about to change your role!");
+    let newRole = prompt("enter your new role:");
 
-   
-    let newRole = prompt("Enter your new role:");
-
-    
-    if (newRole !== null && newRole.trim() !== "") {
-        document.getElementById("role").innerText = newRole;
-        alert("Role updated successfully!");
+    if (newRole && newRole.trim() != "") {
+        document.querySelector("h1").innerText = newRole;
     } else {
-        alert("No changes made.");
+        alert("please enter a valid role!");
     }
 }
